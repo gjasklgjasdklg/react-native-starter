682e44840ae9bc7ea940adda492682e8960c8da4
gjasklgjasdklg <18colemanto@olympustrust.co.uk>
2022-03-11 09:06:26 +0000

    add code

--- App.js
+++ App.js
@@ -1,39 +1,82 @@
-import { Provider } from 'react-redux';
-import React from 'react';
-import { View, ActivityIndicator, StyleSheet } from 'react-native';
-import { PersistGate } from 'redux-persist/integration/react';
-import { NavigationContainer } from '@react-navigation/native';
-import { colors } from './src/styles';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import DatePicker from 'react-native-date-picker';
 
-import { store, persistor } from './src/redux/store';
+function LoginPage() {
+  const [text, setText] = useState('');
+  const [date, setDate] = useState(new Date());
 
-import AppView from './src/modules/AppViewContainer';
-
-export default function App() {
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <PersistGate
-          loading={
-            // eslint-disable-next-line react/jsx-wrap-multilines
-            <View style={styles.container}>
-              <ActivityIndicator color={colors.red} />
-            </View>
-          }
-          persistor={persistor}
-        >
-          <AppView />
-        </PersistGate>
-      </NavigationContainer>
-    </Provider>
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.center}>Enter your User Code:</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={newText => setText(newText)}
+          defaultValue={text}
+        />
+        <Text style={[styles.marginTop20, styles.redText]}>
+          Error Placeholder
+        </Text>
+      </View>
+      <View>
+        <Text style={[styles.center, styles.marginTop50]}>
+          Enter your date of birth:
+        </Text>
+        <DatePicker
+          date={date}
+          onDateChange={setDate}
+          fadeToColor={'none'}
+          mode={'date'}
+        />
+      </View>
+    </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Log in to ClassCharts" component={LoginPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white',
+    flexDirection: 'column',
+  },
+
+  textInput: {
+    borderWidth: 1,
+    width: 200,
+    marginTop: 5,
+  },
+
+  center: {
+    textAlign: 'center',
+  },
+
+  marginTop50: {
+    marginTop: 50,
+  },
+
+  marginTop20: {
+    marginTop: 20,
+  },
+
+  redText: {
+    color: 'red',
   },
 });
+
+export default App; 
